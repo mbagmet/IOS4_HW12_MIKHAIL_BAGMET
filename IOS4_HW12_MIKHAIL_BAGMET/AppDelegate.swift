a555280fac29984d1f9ff6247b512a0e0d3c7562
Mikhail Bagmet <m.bagmet@gmail.com>
2021-12-09 22:36:57 +0200

    Delete Main.storyboard and ViewController.swift

--- IOS4_HW12_MIKHAIL_BAGMET/AppDelegate.swift
+++ IOS4_HW12_MIKHAIL_BAGMET/AppDelegate.swift
@@ -15,9 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = SettingsViewController()
         window?.makeKeyAndVisible()
-
         window?.rootViewController = UINavigationController(rootViewController: SettingsViewController())
 
         return true
