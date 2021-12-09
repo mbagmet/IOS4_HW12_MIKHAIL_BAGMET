6df276381e850e2de53d3b80236bec473c54a033
Mikhail Bagmet <m.bagmet@gmail.com>
2021-12-09 18:46:39 +0200

    Initial Commit

---
+++ IOS4_HW12_MIKHAIL_BAGMET/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS4_HW12_MIKHAIL_BAGMET
+//
+//  Created by Михаил on 09.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
