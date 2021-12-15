e71aa521d42f3780705832a567ce510ddbea590d
Mikhail Bagmet <m.bagmet@gmail.com>
2021-12-15 20:21:40 +0200

    Delete comments

--- IOS4_HW12_MIKHAIL_BAGMET/SettingsViewController.swift
+++ IOS4_HW12_MIKHAIL_BAGMET/SettingsViewController.swift
@@ -61,17 +61,6 @@ class SettingsViewController: UIViewController {
         navigationItem.title = "Настройки"
         self.navigationController?.navigationBar.prefersLargeTitles = true
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 // MARK: - Расширение, чтобы рассказать таблице, откуда брать данные
@@ -79,12 +68,10 @@ class SettingsViewController: UIViewController {
 extension SettingsViewController: UITableViewDataSource {
 
     func numberOfSections(in tableView: UITableView) -> Int {
-        //print("Количество групп: \(settings.count)")
         return settings.count
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        //print("Количество строк в группе: \(settings[section].count)")
         return settings[section].count
     }
 
