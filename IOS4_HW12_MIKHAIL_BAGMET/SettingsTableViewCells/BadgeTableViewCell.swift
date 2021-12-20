359a4d943402aacf6f0963bd2e0601a27680a973
Mikhail Bagmet <m.bagmet@gmail.com>
2021-12-20 16:52:36 +0200

    Add Table cell with badge

--- IOS4_HW12_MIKHAIL_BAGMET/SettingsTableViewCells/BadgeTableViewCell.swift
+++ IOS4_HW12_MIKHAIL_BAGMET/SettingsTableViewCells/BadgeTableViewCell.swift
@@ -9,15 +9,54 @@ import UIKit
 
 class BadgeTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    static let identifier = "BadgeTableViewCell"
+
+    lazy var badgeLabel: UILabel = {
+        let badge = UILabel()
+        badge.backgroundColor = .systemRed
+        badge.textColor = .white
+        badge.textAlignment = .center
+        badge.layer.masksToBounds = true
+        badge.layer.cornerRadius = Metric.badgeHeight / 2
+
+        let digitsQty: CGFloat = 1
+        let width = max(Metric.badgeHeight, 0.6 * Metric.badgeHeight * digitsQty)
+        badge.widthAnchor.constraint(equalToConstant: width).isActive = true
+
+        return badge
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .value1, reuseIdentifier: reuseIdentifier)
+
+        setupHierarchy()
+        setupLayout()
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 
+    // MARK: - Settings
+
+    private func setupHierarchy() {
+        self.contentView.addSubview(badgeLabel)
+    }
+
+    private func setupLayout() {
+        self.textLabel?.addConstraints(centerY: contentView.centerYAnchor, left: self.leadingAnchor, paddingLeft: 65)
+        self.textLabel?.trailingAnchor.constraint(lessThanOrEqualTo: badgeLabel.leadingAnchor, constant: -6).isActive = true
+
+        badgeLabel.addConstraints(centerY: contentView.centerYAnchor, right: contentView.trailingAnchor, paddingRight: 6, height: Metric.badgeHeight)
+
+        self.imageView?.layer.masksToBounds = true
+        self.imageView?.layer.cornerRadius = 7
+    }
+}
+
+extension BadgeTableViewCell {
+    enum Metric {
+        static let badgeHeight: CGFloat = 26
+        static let badgeWidth: CGFloat = 26
+    }
 }
