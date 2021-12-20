8556af4ceea2acb26b3f931a61153c66d966d7d4
Mikhail Bagmet <m.bagmet@gmail.com>
2021-12-20 16:52:16 +0200

    Add table cell with badge

---
+++ IOS4_HW12_MIKHAIL_BAGMET/SettingsTableViewCells/BadgeTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  BadgeTableViewCell.swift
+//  IOS4_HW12_MIKHAIL_BAGMET
+//
+//  Created by Михаил on 18.12.2021.
+//
+
+import UIKit
+
+class BadgeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
